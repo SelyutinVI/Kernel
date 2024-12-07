f718370f2ee3892fb2ba1b212d6d5e8281de555d
RealBlank / <selyutinvi@gmail.com>
2024-12-07 19:41:19 +0300

    Release 0.2.0: Commands and queries interfaces, base classes for aggregate and specification

---
+++ src/Kernel/BaseAggregate.cs
@@ -0,0 +1,7 @@
+﻿namespace RealBlank.Kernel;
+
+public abstract class BaseAggregate(Guid? uuid) : IAggregate
+{
+    public Guid UUID { get; init; } = uuid ?? Guid.NewGuid();
+}
+
---
+++ src/Kernel/BaseSpecification.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+
+namespace RealBlank.Kernel;
+
+public abstract class BaseSpecification<T> : ISpecification<T>
+{
+    public Expression<Func<T, bool>>? Query { get; protected set; }
+
+    public bool IsSatisfiedBy(T inner)
+    {
+        if (inner == null) 
+            throw new ArgumentNullException(nameof(inner));
+
+        if (Query == null)
+            return false;
+
+        var func = Query.Compile();
+        return func(inner);
+    }
+}
